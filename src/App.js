f6500f3ed77b75ba6591718bce73ace826b2036e
Asomiddin <asomhon12345@gmail.com>
2022-05-20 11:41:23 +0500

    progress voice-app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React from 'react';
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 function App() {
+    const commands = [
+        {
+            command : 'open *',
+            callback: (site)=>{
+                window.open('https://'+site)
+            }
+        } ,
+        {
+            command : 'background color *',
+            callback: (color)=>{
+                document.body.style.background = color;
+            }
+        }
+    ]
+    const {
+        transcript,
+        listening,
+        resetTranscript,
+        browserSupportsSpeechRecognition
+    } = useSpeechRecognition({commands});
+
+    if (!browserSupportsSpeechRecognition) {
+        return <span>Browser doesn't support speech recognition.</span>;
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className={'container'}>
+         <div className="header">
+          <button onClick={SpeechRecognition.startListening({continuous:true,language:'uz'})} >
+              <i className="bi bi-mic" style={{color:`${listening ? 'blue' : 'green'}`}}> </i>
+          </button>
+          <button onClick={SpeechRecognition.stopListening}>
+              <i className="bi bi-mic-mute"> </i>
+          </button>
+          <button onClick={resetTranscript}>
+              <i className="bi bi-arrow-clockwise"> </i>
+          </button>
+         </div>
+          <div className={'block'}>
+
+              <p>{transcript}</p>
+          </div>
+      </div>
   );
 }
 
